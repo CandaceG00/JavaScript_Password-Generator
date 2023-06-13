60afca1fbcc57ddabb6914df1b2291763c8fc3aa
G <cmgostinski00@gmail.com>
2023-06-12 21:00:27 -0400

    added more special characters and modified vars

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,10 @@ var characterLength = 8;
 var choiceArr = [];
 
 var specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '/', '<', '>', '[', ']', '{', '}', '?'];
-var lowerCase = [];
-var upperCase = [];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
