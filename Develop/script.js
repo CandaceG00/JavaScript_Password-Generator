fc5591cfda735a159796a9e798ff23275fc8fff2
G <cmgostinski00@gmail.com>
2023-06-15 10:39:58 -0400

    added if statements to the arrays for valid characters.

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,19 @@
-var characterLength = 8;
-var choiceArr = [];
+let characterLength = 8;
+let choiceArr = [];
 
-var specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '/', '<', '>', '[', ']', '{', '}', '?'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+let specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '/', '<', '>', '[', ']', '{', '}', '?'];
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
@@ -24,10 +24,30 @@ function generatePassword () {
 
 function getPrompts () {
   characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters"));
-  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128)
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert ("Character length has to be a number between 8 - 128 digits. Please try again.");
     return false;
 }
+
+if (confirm("Would you like lowercase letters in your password?")) {
+  choiceArr = choiceArr.concat(lowerCase);
+}
+
+if (confirm("Would you like uppercase letters in your password?")) {
+  choiceArr = choiceArr.concat(upperCase);  
+}
+
+if (confirm("Would you like special characters in your password?")) {
+  choiceArr = choiceArr.concat(specialCharacter); 
+}
+
+if (confirm("Would you like numbers in your password?")) {
+  choiceArr = choiceArr.concat(numberArr); 
+}
+return true;
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
